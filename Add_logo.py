4d5fcd8734145fee3c2914b19334dedf21f65710
300841 <300841@shakopeeschools.org>
2018-03-02 13:49:15 -0600

    added images

--- Add_logo.py
+++
@@ -1,8 +0,0 @@
-import PIL
-import matplotlib.pyplot as plt # single use of plt is commented out
-import os.path  
-import PIL.ImageDraw 
-
-def add_logo(original_image, logo):
-    '''adds a logo to the bottom right corner of all images'''
-    
